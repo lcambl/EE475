bd36b34d7c591a980533b23f2bb8e56767e73f14
Lucy Camblin <lcambl@uw.edu>
2023-11-06 14:52:59 -0800

    working sensor readout

--- readLoadCell.ino
+++ readLoadCell.ino
@@ -5,12 +5,13 @@ BluetoothSerial SerialBT;
 HX711 scale;
 const byte loadcell_dout = 23;
 const byte loadcell_sck = 22;
+
 // Variables for the moving average filter
-const int numReadings = 5;
+const int numReadings = 1;
 long readings[numReadings]; // the readings from the load cell
-int readIndex = 0; // the index of the current reading
-long total = 0; // the running total
-long average = 0; // the average
+int samples = 0; // the index of the current reading
+long runningTotal = 0; 
+long threshold = 40000;
 
 // Calibration variables
 long calibrationFactor = 1; // this calibration factor is obtained through calibration
@@ -23,61 +24,18 @@ void setup() {
   scale.begin(loadcell_dout, loadcell_sck);
   scale.set_scale();
   scale.tare();
-  // Initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (scale.is_ready()) {
-    SerialBT.println("scale was ready");
-    long reading = scale.read();
-    SerialBT.println(reading);
-    // Subtract the last reading:
-    total = total - readings[readIndex];
-    // Read from the sensor:
-    readings[readIndex] = reading;
-    // Add the reading to the total:
-    total = total + readings[readIndex];
-    // Advance to the next position in the array:
-    readIndex = readIndex + 1;
-
-    // If we're at the end of the array...
-    if (readIndex >= numReadings) {
-      // ...wrap around to the beginning:
-      readIndex = 0;
-    }
-
-    // Calculate the average:
-    average = total / numReadings;
-    // Scale the average reading to the known weight to get the calibration factor
-    if (scale.get_units(10) != 0) {
-      calibrationFactor = knownWeight / scale.get_units(10);
-      scale.set_scale(calibrationFactor); // Adjust to this calibration factor
-    }  
-
-    // // Print out the raw, average, and the weight readings:
-    // Serial.print("Raw reading: ");
-    // Serial.print(reading);
-    // Serial.print(" Average reading: ");
-    // Serial.print(average);
-    // Serial.print(" Weight: ");
-    // Serial.print(scale.get_units(), 2); // two decimal places
-    // Serial.println(" g");
-
-  } else {
-    SerialBT.println("HX711 not found.");
+  while (! scale.is_ready()) {
+    
+  } 
+  // read from sensor
+  long reading = scale.read();
+  // print raw reading to BT device's console
+  if (reading > threshold) {
+    SerialBT.println("Threshold exceeded, sensor value:");
+    SerialBT.println(reading); 
   }
-
-  delay(1000);
-  // if (Serial.available()) {
-  //   SerialBT.write(Serial.read());
-
-  // }
-  // if (SerialBT.available()) {
-  //   Serial.write(SerialBT.read());
-  // }
-  // delay(20);
 }
